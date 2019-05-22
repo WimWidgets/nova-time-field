89c3554a9fb91a7a89d7f7460bd5b37ce30c6b0f
Wim <wim@widgets.nl>
2019-05-22 15:21:14 +0200

    Added config file with time formats

---
+++ src/config/nova-time-field.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Input Format
+    |--------------------------------------------------------------------------
+    |
+    | The PHP date format used to input data in the field. This is the format
+    | which determines the expected user input.
+    |
+    */
+
+    'input-format' => 'H:i',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Output Format
+    |--------------------------------------------------------------------------
+    |
+    | The PHP date format used to output data from the field. This is the
+    | format your model (and database) will receive.
+    |
+    */
+
+    'output-format' => 'H:i',
+
+];
